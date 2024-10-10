2dfda516264ff9502fcb329dbdc725823568445e
lanpia <95679243+lanpia@users.noreply.github.com>
2024-10-10 23:44:34 +0900

    .

--- Piscine0/ex04/whatis.py
+++ Piscine0/ex04/whatis.py
@@ -1,24 +1,22 @@
 import sys
 
 def check_odd_or_even():
-		# 인자가 하나인지 확인
-		if len(sys.argv) != 2:
-				raise AssertionError("more than one argument is provided" \
-														if len(sys.argv) > 2 \
-														else "argument is not an integer")
-
-		try:
-				# 문자열을 정수로 변환 시도
-				num = int(sys.argv[1])
-		except ValueError:
-				# 정수로 변환할 수 없는 경우
-				raise AssertionError("argument is not an integer")
-
-		# 홀수, 짝수 여부 확인
-		if num % 2 == 0:
-				print("I'm Even.")
-		else:
-				print("I'm Odd.")
+	# assert len(sys.argv) == 2, "argument is not an integer"
+	# assert len(sys.argv) <= 2, "more than one argument is provided"
+    try:
+        if len(sys.argv) < 2:
+            raise AssertionError("argument is not an integer")
+        elif len(sys.argv) > 2:
+            raise AssertionError("more than one argument is provided")
+        num = int(sys.argv[1])
+        if num % 2 == 0:
+            print("I'm Even.")
+        else:
+            print("I'm Odd.")
+    except AssertionError as e:
+        print(f"AssertionError: {e}")
+    except ValueError:
+        print("AssertionError: argument is not an integer")
 
 if __name__ == "__main__":
-		check_odd_or_even()
+    check_odd_or_even()
