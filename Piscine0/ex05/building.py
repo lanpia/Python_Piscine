903c516bed6dc5f486ad913e348a77f3e7ba65f1
lanpia <roina1108@gmail.com>
2024-10-25 16:27:40 +0900

    .

--- Piscine0/ex05/building.py
+++ Piscine0/ex05/building.py
@@ -2,28 +2,28 @@ import sys
 import string
 
 def count_characters(text: str) -> None:
-    upper_count = sum(1 for char in text if char.isupper())
-    lower_count = sum(1 for char in text if char.islower())
-    digit_count = sum(1 for char in text if char.isdigit())
-    space_count = sum(1 for char in text if char.isspace())
-    punctuation_count = sum(1 for char in text if char in string.punctuation)
+	upper_count = sum(1 for char in text if char.isupper())
+	lower_count = sum(1 for char in text if char.islower())
+	digit_count = sum(1 for char in text if char.isdigit())
+	space_count = sum(1 for char in text if char.isspace())
+	punctuation_count = sum(1 for char in text if char in string.punctuation)
 
-    print(f"The text contains {len(text)} characters:")
-    print(f"{upper_count} upper letters")
-    print(f"{lower_count} lower letters")
-    print(f"{punctuation_count} punctuation marks")
-    print(f"{space_count} spaces")
-    print(f"{digit_count} digits")
+	print(f"The text contains {len(text)} characters:")
+	print(f"{upper_count} upper letters")
+	print(f"{lower_count} lower letters")
+	print(f"{punctuation_count} punctuation marks")
+	print(f"{space_count} spaces")
+	print(f"{digit_count} digits")
 
 def main():
-    if len(sys.argv) == 1:
-        text = input("What is the text to count?\n")
-    elif len(sys.argv) == 2:
-        text = sys.argv[1]
-    else:
-        raise AssertionError("more than one argument is provided")
+	if len(sys.argv) == 1:
+			text = input("What is the text to count?\n")
+	elif len(sys.argv) == 2:
+			text = sys.argv[1]
+	else:
+			raise AssertionError("more than one argument is provided")
 
-    count_characters(text)
+	count_characters(text)
 
 if __name__ == "__main__":
-    main()
+	main()
