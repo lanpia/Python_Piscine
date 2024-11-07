e45f26b9f3325e20d5050d6d82a183cdb30c6c11
lanpia <95679243+lanpia@users.noreply.github.com>
2024-11-07 17:11:58 +0900

    2024-11-07 17:11

--- Piscine1/ex00/give_bmi.py
+++ Piscine1/ex00/give_bmi.py
@@ -1,7 +1,9 @@
 import numpy as np
 
-def give_bmi(height: list[int | float], weight: list[int | float]) -> list[int | float]:
-    """주어진 신장(height)과 체중(weight)을 통해 각각의 BMI를 계산하여 numpy 배열로 반환합니다."""
+def give_bmi(height: list[int | float],
+            weight: list[int | float]) -> list[int | float]:
+    """주어진 신장(height)과 체중(weight)을 통해 각각의 BMI를 계산하여
+    numpy 배열로 반환합니다."""
     height_array = np.array(height, dtype=float)
     weight_array = np.array(weight, dtype=float)
     if height_array.size != weight_array.size:
@@ -11,5 +13,6 @@ def give_bmi(height: list[int | float], weight: list[int | float]) -> list[int |
 
 
 def apply_limit(bmi: list[int | float], limit: int) -> list[bool]:
-    """BMI numpy 배열과 제한값(limit)을 받아 제한을 초과하는 경우 True, 그렇지 않으면 False를 반환하는 numpy 배열을 생성합니다."""
+    """BMI numpy 배열과 제한값(limit)을 받아 제한을 초과하는 경우 True,
+    그렇지 않으면 False를 반환하는 numpy 배열을 생성합니다."""
     return bmi > limit
